c7a94a236f3141087e2b6762d3416a8534a43716
Hernan Aymard <heraym@hotmail.com>
2024-01-22 20:13:35 +0000

    Analizar Imagen

--- vertex-ai/apps/analizarImagen/server.js
+++ vertex-ai/apps/analizarImagen/server.js
@@ -1,20 +1,10 @@
 const fs = require("fs");
 const {VertexAI} = require('@google-cloud/vertexai');
 
-var projectId = 'PROJECT_ID';
+var projectId = 'projectodemos';
 var location = 'us-central1';
 var model = 'gemini-pro-vision';
 var image = 'gs://generativeai-downloads/images/scones.jpg';
-var mimeType = 'image/jpeg';
-
-
-// Initialize Vertex with your Cloud project and location
-const vertexAI = new VertexAI({project: projectId, location: location});
-
-// Instantiate the model
-const generativeVisionModel = vertexAI.preview.getGenerativeModel({
-  model: model,
-});
 
 
 const http = require('http');
@@ -34,49 +24,44 @@ app.set("view engine", "ejs");
 
 const server = http.createServer(app);
 
-var imagen = "images/imagen1.jpg";
-
-
-
-  
-  app.get('/text-only', function(req,res,next) {
-      res.render("text-only",{ prompt: "Escribe una historia sobre una mochila magica."});
-  }); 
-  app.get('/text-only-query', async function(req,res,next) {
-    // For text-only input, use the gemini-pro model
-   
-    const prompt = req.query.prompt;
-  
-   
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = await response.text();
-    console.log(text);
-    res.send(text);
-  }); 
+var imagen = "./images/imagen1.jpg";
+var mimeType = 'image/jpeg';
+var prompt = "what is shown in this image?";
   
   app.get('/analizar-imagen', function(req,res,next) {
       res.render("analizar-imagen",{ imagen: "images/imagen1.jpg"});
   });
 
   app.get('/analizar-imagen-query', async function(req,res,next) {
- 
+
+   prompt = req.query.prompt;
+   const contenidoImagen = await fs.readFileSync(imagen, {encoding: 'base64'});
+   // console.log(contenidoImagen);
+    
     // For images, the SDK supports both Google Cloud Storage URI and base64 strings
    const filePart = {
-     fileData: {
-       fileUri: imagen,
+     inlineData: {
+       data: contenidoImagen,
        mimeType: mimeType,
      },
    };
 
   const textPart = {
-     text: 'what is shown in this image?',
+     text: prompt
   };
 
   const request = {
     contents: [{role: 'user', parts: [filePart, textPart]}],
   };
    
+  // Initialize Vertex with your Cloud project and location
+const vertexAI = new VertexAI({project: projectId, location: location});
+
+// Instantiate the model
+const generativeVisionModel = vertexAI.preview.getGenerativeModel({
+  model: model,
+});
+
    // Create the response stream
   const responseStream =
     await generativeVisionModel.generateContentStream(request);
@@ -98,6 +83,7 @@ var imagen = "images/imagen1.jpg";
       form.parse(req, function (err, fields, files) {
           var oldpath = files.filetoupload[0].filepath;
           var newpath = 'images/' + files.filetoupload[0].originalFilename;
+          mimeType = files.filetoupload[0].mimetype;
           imagen = newpath;
         fs.copyFile(oldpath, newpath, function (err) {
           if (err) throw err;
